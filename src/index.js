68fec49696c341b8917947559a92a2cd73a4c33f
Woobin Han <woobin.han23@gmail.com>
2023-03-17 18:42:56 -0400

    imported myName function to index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = myName("Woobin");
+  return element;
+}
   
-    return element;
-  }
-  
-  document.body.appendChild(component());
+document.body.appendChild(component());
