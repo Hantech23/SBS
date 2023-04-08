1e4486e743bd288a5af1b711bb75a418bb4fc4ee
Woobin Han <woobin.han23@gmail.com>
2023-04-08 16:57:19 -0400

    created logo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import _ from 'lodash';
 import './style.css';
 
-
 function logo() {
-  const star = document.createElement('div');
-  star.innerHTML = "STAR";
-  star.classList.add('star');
+const star = document.createElement('div');
+star.innerHTML = "STAR";
+star.classList.add('star');
+document.body.appendChild(star);
+const beauty = document.createElement('div');
+beauty.innerHTML = "BEAUTY";
+beauty.classList.add('beauty');
+document.body.appendChild(beauty);
+const supply = document.createElement('div');
+supply.innerHTML = "SUPPLY";
+supply.classList.add('supply');
+document.body.appendChild(supply);
+}
+logo();
+
+
+
+
+
+  /*
+  function logo() {
+  
 
   const beauty = document.createElement('div');
   beauty.innerHTML = "BEAUTY";
@@ -13,10 +31,14 @@ function logo() {
   const supply = document.createElement('div');
   supply.innerHTML = "SUPPLY";
 
-  return star;
+  return beauty;
 }
 
-document.body.appendChild(logo());
+document.body.appendChild(document.createElement('div'));
+
+*/
+
+
 /*
 
 function test() {
