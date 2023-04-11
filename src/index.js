549b81f9e6c714c8ba141a1f98908264355a6aaa
Woobin Han <woobin.han23@gmail.com>
2023-04-11 09:34:29 -0400

    added margin

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import _ from 'lodash';
 import './style.css';
 
 function logo() {
+
+const logoParent = document.createElement('div');
+logoParent.classList.add('logoParent');
+document.body.appendChild(logoParent);
 const star = document.createElement('div');
 star.innerHTML = "STAR";
 star.classList.add('star');
-document.body.appendChild(star);
+logoParent.appendChild(star);
 const beauty = document.createElement('div');
 beauty.innerHTML = "BEAUTY";
 beauty.classList.add('beauty');
-document.body.appendChild(beauty);
+logoParent.appendChild(beauty);
 const supply = document.createElement('div');
 supply.innerHTML = "SUPPLY";
 supply.classList.add('supply');
-document.body.appendChild(supply);
+logoParent.appendChild(supply);
 }
 logo();
 
