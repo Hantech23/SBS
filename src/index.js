e398f896a75c233a5724b536365cc7279b55589a
Woobin Han <woobin.han23@gmail.com>
2023-04-15 05:27:33 -0400

    added drop down menu

--- src/index.js
+++ src/index.js
@@ -25,6 +25,38 @@ logoParent.appendChild(supply);
 }
 logo(header);
 
+const dropDownMenu = document.createElement('div');
+dropDownMenu.classList.add('dropDownMenu');
+document.body.appendChild(dropDownMenu);
+
+function dropMenu(location) {
+  const hair = document.createElement('div');
+  hair.innerHTML = "Hair";
+  hair.classList.add('hair');
+  location.appendChild(hair);
+  const hairProducts = document.createElement('div');
+  hairProducts.innerHTML = "Haircare Products";
+  hairProducts.classList.add('hairProducts');
+  location.appendChild(hairProducts);
+  const cosmetics = document.createElement('div');
+  cosmetics.innerHTML = "Cosmetics";
+  cosmetics.classList.add('cosmetics');
+  location.appendChild(cosmetics);
+  const skincare = document.createElement('div');
+  skincare.innerHTML = "Skincare";
+  skincare.classList.add('skincare');
+  location.appendChild(skincare);
+  const services = document.createElement('div');
+  services.innerHTML = "Services";
+  services.classList.add('services');
+  location.appendChild(services);
+  const accessories = document.createElement('div');
+  accessories.innerHTML = "Accessories";
+  accessories.classList.add('accessories');
+  location.appendChild(accessories);
+}
+dropMenu(dropDownMenu);
+
 const bodyImage = new Image();
 bodyImage.src = frontImage;
 bodyImage.classList.add('bodyImage');
