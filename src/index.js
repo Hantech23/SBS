ee08b5d51debb0a0619c9eeec5fda52f95b60ea0
Woobin Han <woobin.han23@gmail.com>
2023-05-23 00:39:42 -0400

    finished hair sub menu

--- src/index.js
+++ src/index.js
@@ -91,8 +91,7 @@ function dropMenu(location) {
   hairSub1Item3Link.appendChild(hairSub1Item3LinkText);
   hairSub1Item3Link.href = "";
   hairSub1Item3.appendChild(hairSub1Item3Link);
-
-  //VERA LOSA
+  //VERA LOSA 8A 100% FULL LACE Wigs
   const hairSub2 = document.createElement('li');
   hairSub.appendChild(hairSub2);
   const hairSub2Link = document.createElement('a');
@@ -106,6 +105,7 @@ function dropMenu(location) {
   hairSub2.appendChild(hairSub2ItemsContainer);
   const hairSub2Items = document.createElement('ul');
   hairSub2ItemsContainer.appendChild(hairSub2Items);
+
   const hairSub2Item1 = document.createElement('li');
   hairSub2Items.appendChild(hairSub2Item1);
   const hairSub2Item1Link = document.createElement('a');
@@ -113,6 +113,7 @@ function dropMenu(location) {
   hairSub2Item1Link.appendChild(hairSub2Item1LinkText);
   hairSub2Item1Link.href = "";
   hairSub2Item1.appendChild(hairSub2Item1Link);
+
   const hairSub2Item2 = document.createElement('li');
   hairSub2Items.appendChild(hairSub2Item2);
   const hairSub2Item2Link = document.createElement('a');
@@ -120,6 +121,120 @@ function dropMenu(location) {
   hairSub2Item2Link.appendChild(hairSub2Item2LinkText);
   hairSub2Item2Link.href = "";
   hairSub2Item2.appendChild(hairSub2Item2Link);
+  //VERA LOSA 9A 100% BRAZILIAN VIRGIN HAIR
+  const hairSub3 = document.createElement('li');
+  hairSub.appendChild(hairSub3);
+  const hairSub3Link = document.createElement('a');
+  const hairSub3LinkText = document.createTextNode("VERA LOSA™ 9A 100% BRAZILIAN VIRGIN HAIR");
+  hairSub3Link.appendChild(hairSub3LinkText);
+  hairSub3Link.href = "";
+  hairSub3.appendChild(hairSub3Link);
+
+  const hairSub3ItemsContainer = document.createElement('div');
+  hairSub3ItemsContainer.classList.add('menu-item');
+  hairSub3.appendChild(hairSub3ItemsContainer);
+  const hairSub3Items = document.createElement('ul');
+  hairSub3ItemsContainer.appendChild(hairSub3Items);
+
+  const hairSub3Item1 = document.createElement('li');
+  hairSub3Items.appendChild(hairSub3Item1);
+  const hairSub3Item1Link = document.createElement('a');
+  const hairSub3Item1LinkText = document.createTextNode("STRAIGHT");
+  hairSub3Item1Link.appendChild(hairSub3Item1LinkText);
+  hairSub3Item1Link.href = "";
+  hairSub3Item1.appendChild(hairSub3Item1Link);
+  
+  const hairSub3Item2 = document.createElement('li');
+  hairSub3Items.appendChild(hairSub3Item2);
+  const hairSub3Item2Link = document.createElement('a');
+  const hairSub3Item2LinkText = document.createTextNode("WAVY");
+  hairSub3Item2Link.appendChild(hairSub3Item2LinkText);
+  hairSub3Item2Link.href = "";
+  hairSub3Item2.appendChild(hairSub3Item2Link);
+
+  const hairSub3Item3 = document.createElement('li');
+  hairSub3Items.appendChild(hairSub3Item3);
+  const hairSub3Item3Link = document.createElement('a');
+  const hairSub3Item3LinkText = document.createTextNode("CURLY");
+  hairSub3Item3Link.appendChild(hairSub3Item3LinkText);
+  hairSub3Item3Link.href = "";
+  hairSub3Item3.appendChild(hairSub3Item3Link);
+  //VERA LOSA 8A 100% BRAZILIAN VIRGIN HAIR
+  const hairSub4 = document.createElement('li');
+  hairSub.appendChild(hairSub4);
+  const hairSub4Link = document.createElement('a');
+  const hairSub4LinkText = document.createTextNode("VERA LOSA™ 8A 100% BRAZILIAN VIRGIN HAIR");
+  hairSub4Link.appendChild(hairSub4LinkText);
+  hairSub4Link.href = "";
+  hairSub4.appendChild(hairSub4Link);
+
+  const hairSub4ItemsContainer = document.createElement('div');
+  hairSub4ItemsContainer.classList.add('menu-item');
+  hairSub4.appendChild(hairSub4ItemsContainer);
+  const hairSub4Items = document.createElement('ul');
+  hairSub4ItemsContainer.appendChild(hairSub4Items);
+
+  const hairSub4Item1 = document.createElement('li');
+  hairSub4Items.appendChild(hairSub4Item1);
+  const hairSub4Item1Link = document.createElement('a');
+  const hairSub4Item1LinkText = document.createTextNode("STRAIGHT");
+  hairSub4Item1Link.appendChild(hairSub4Item1LinkText);
+  hairSub4Item1Link.href = "";
+  hairSub4Item1.appendChild(hairSub4Item1Link);
+
+  const hairSub4Item2 = document.createElement('li');
+  hairSub4Items.appendChild(hairSub4Item2);
+  const hairSub4Item2Link = document.createElement('a');
+  const hairSub4Item2LinkText = document.createTextNode("WAVY");
+  hairSub4Item2Link.appendChild(hairSub4Item2LinkText);
+  hairSub4Item2Link.href = "";
+  hairSub4Item2.appendChild(hairSub4Item2Link);
+  //CROCHET BRAID
+  const hairSub5 = document.createElement('li');
+  hairSub.appendChild(hairSub5);
+  const hairSub5Link = document.createElement('a');
+  const hairSub5LinkText = document.createTextNode("CHROCHET BRAID");
+  hairSub5Link.appendChild(hairSub5LinkText);
+  hairSub5Link.href = "";
+  hairSub5.appendChild(hairSub5Link);
+
+  const hairSub5ItemsContainer = document.createElement('div');
+  hairSub5ItemsContainer.classList.add('menu-item');
+  hairSub5.appendChild(hairSub5ItemsContainer);
+  const hairSub5Items = document.createElement('ul');
+  hairSub5ItemsContainer.appendChild(hairSub5Items);
+
+  const hairSub5Item1 = document.createElement('li');
+  hairSub5Items.appendChild(hairSub5Item1);
+  const hairSub5Item1Link = document.createElement('a');
+  const hairSub5Item1LinkText = document.createTextNode("CHROCHET HAIR");
+  hairSub5Item1Link.appendChild(hairSub5Item1LinkText);
+  hairSub5Item1Link.href = "";
+  hairSub5Item1.appendChild(hairSub5Item1Link);
+
+  const hairSub5Item2 = document.createElement('li');
+  hairSub5Items.appendChild(hairSub5Item2);
+  const hairSub5Item2Link = document.createElement('a');
+  const hairSub5Item2LinkText = document.createTextNode("PRE-STRETCHED CHROCHET HAIR");
+  hairSub5Item2Link.appendChild(hairSub5Item2LinkText);
+  hairSub5Item2Link.href = "";
+  hairSub5Item2.appendChild(hairSub5Item2Link);
+
+  const hairSub5Item3 = document.createElement('li');
+  hairSub5Items.appendChild(hairSub5Item3);
+  const hairSub5Item3Link = document.createElement('a');
+  const hairSub5Item3LinkText = document.createTextNode("LOCS");
+  hairSub5Item3Link.appendChild(hairSub5Item3LinkText);
+  hairSub5Item3Link.href = "";
+  hairSub5Item3.appendChild(hairSub5Item3Link);
+
+  const hairSub5Item4 = document.createElement('li');
+  hairSub5Items.appendChild(hairSub5Item4);
+  const hairSub5Item4Link = document.createElement('a');
+  const hairSub5Item4LinkText = document.createTextNode("TWISTS");
+  hairSub5Item4Link.appendChild(hairSub5Item4LinkText);
+  hairSub5Item4Link.href = "";
+  hairSub5Item4.appendChild(hairSub5Item4Link);
 
 
   //HAIR CARE
@@ -137,9 +252,10 @@ function dropMenu(location) {
 
   const hairCareSub = document.createElement('ul');
   hairCareMenuSub.appendChild(hairCareSub);
+  
+  //PROFESSIONAL HAIR CARE
   const hairCareSub1 = document.createElement('li');
   hairCareSub.appendChild(hairCareSub1);
-
   const hairCareSub1Link = document.createElement('a');
   const hairCareSub1LinkText = document.createTextNode("PROFESSIONAL HAIR CARE");
   hairCareSub1Link.appendChild(hairCareSub1LinkText);
@@ -148,23 +264,57 @@ function dropMenu(location) {
 
   const hairCareSub1ItemsContainer = document.createElement('div');
   hairCareSub1ItemsContainer.classList.add('menu-item');
-  
+  hairCareSub1.appendChild(hairCareSub1ItemsContainer);
+  const hairCareSub1Items = document.createElement('ul');
+  hairCareSub1ItemsContainer.appendChild(hairCareSub1Items);
 
-  /*
-  
-  
-  
-  const hairCareSub = document.createElement('ul');
-  hairCareMenuSub.appendChild(hairCareSub);
-  const hairCareSub1 = document.createElement('li');
-  hairCareSub.appendChild(hairCareSub1);
-  const hairCareSub1Link = document.createElement('a');
-  const hairCareSub1LinkText = document.createTextNode("PROFESSIONAL HAIR CARE");
-  hairCareSub1Link.appendChild(hairCareSub1LinkText);
-  hairCareSub1Link.href = "";
-  hairCareSub1.appendChild(hairSub1Link);
-  */
+  const hairCareSub1Item1 = document.createElement('li');
+  hairCareSub1Items.appendChild(hairCareSub1Item1);
+  const hairCareSub1Item1Link = document.createElement('a');
+  const hairCareSub1Item1LinkText = document.createTextNode("AMIKA");
+  hairCareSub1Item1Link.appendChild(hairCareSub1Item1LinkText);
+  hairCareSub1Item1Link.href = "";
+  hairCareSub1Item1.appendChild(hairCareSub1Item1Link);
 
+  const hairCareSub1Item2 = document.createElement('li');
+  hairCareSub1Items.appendChild(hairCareSub1Item2);
+  const hairCareSub1Item2Link = document.createElement('a');
+  const hairCareSub1Item2LinkText = document.createTextNode("MATRIX");
+  hairCareSub1Item2Link.appendChild(hairCareSub1Item2LinkText);
+  hairCareSub1Item2Link.href = "";
+  hairCareSub1Item2.appendChild(hairCareSub1Item2Link);
+
+  const hairCareSub1Item3 = document.createElement('li');
+  hairCareSub1Items.appendChild(hairCareSub1Item3);
+  const hairCareSub1Item3Link = document.createElement('a');
+  const hairCareSub1Item3LinkText = document.createTextNode("BIOLAGE");
+  hairCareSub1Item3Link.appendChild(hairCareSub1Item3LinkText);
+  hairCareSub1Item3Link.href = "";
+  hairCareSub1Item3.appendChild(hairCareSub1Item3Link);
+
+  const hairCareSub1Item4 = document.createElement('li');
+  hairCareSub1Items.appendChild(hairCareSub1Item4);
+  const hairCareSub1Item4Link = document.createElement('a');
+  const hairCareSub1Item4LinkText = document.createTextNode("AG HAIR");
+  hairCareSub1Item4Link.appendChild(hairCareSub1Item4LinkText);
+  hairCareSub1Item4Link.href = "";
+  hairCareSub1Item4.appendChild(hairCareSub1Item4Link);
+
+  const hairCareSub1Item5 = document.createElement('li');
+  hairCareSub1Items.appendChild(hairCareSub1Item3);
+  const hairCareSub1Item5Link = document.createElement('a');
+  const hairCareSub1Item5LinkText = document.createTextNode("REDKEN");
+  hairCareSub1Item5Link.appendChild(hairCareSub1Item5LinkText);
+  hairCareSub1Item5Link.href = "";
+  hairCareSub1Item5.appendChild(hairCareSub1Item5Link);
+  //NATURAL SHAMPOOS
+  const hairCareSub2 = document.createElement('li');
+  hairCareSub.appendChild(hairCareSub2);
+  const hairCareSub2Link = document.createElement('a');
+  const hairCareSub2LinkText = document.createTextNode("NATURAL SHAMPOOS");
+  hairCareSub2Link.appendChild(hairCareSub2LinkText);
+  hairCareSub2Link.href = "";
+  hairCareSub2.appendChild(hairCareSub2Link);
 
   const cosmetics = document.createElement('li');
   menuMain.appendChild(cosmetics);
@@ -269,6 +419,10 @@ display(clearanceArrayContainer, clearanceArray);
 const footer = document.createElement('div');
 footer.classList.add('footer');
 document.body.appendChild(footer);
+const footerLogo = document.createElement('div');
+footerLogo.classList.add('footer-logo');
+footerLogo.innerHTML = "© 2023 STAR BEAUTY SUPPLY, All rights reserved."
+footer.appendChild(footerLogo);
 
 
 
